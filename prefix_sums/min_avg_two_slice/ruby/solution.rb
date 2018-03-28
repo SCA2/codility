77837caba839cf7ad6e0cabb7fa7812f1dd7333b
SCA2 <tpryan_1965@yahoo.com>
2018-03-27 19:13:32 -0700

    refactor min_avg_two_slice

--- prefix_sums/min_avg_two_slice/ruby/solution.rb
+++ prefix_sums/min_avg_two_slice/ruby/solution.rb
@@ -3,34 +3,30 @@
 # Find the minimal average of any slice containing at least two elements.
 
 def solution(a)
-  head = 1
-  tail = 0
-  minIndex = tail
-  minAvg = Float::INFINITY
-  nextHeadAvg = minAvg
-  nextTailAvg = minAvg
-  sums = a.reduce([0]) {|sum, e| sum.push(sum[-1] + e) }[1..-1]
+  min_2_index = 0
+  min_3_index = 0
+  min_2 = Float::INFINITY
+  min_3 = Float::INFINITY
 
-  while tail < a.length - 1 do
-    span = head - tail + 1
-    sum = sums[head] - sums[tail] + a[tail]
-
-    nextHeadAvg = (sum + a[head + 1]).fdiv(span + 1) if head < a.length - 1
-    nextTailAvg = (sum - a[tail]).fdiv(span - 1) if tail < head - 1
-
-    if nextHeadAvg < minAvg
-      head += 1
-      minAvg = nextHeadAvg
-      minIndex = tail
-    elsif nextTailAvg < minAvg
-      tail += 1
-      minAvg = nextTailAvg
-      minIndex = tail 
-    else
-      tail += 1
+  for i in 1..a.length - 1 do
+    avg = a[i-1] + a[i]
+    if avg < min_2
+      min_2 = avg
+      min_2_index = i - 1
     end
-    head += 1 if head == tail
   end
-  # puts "tail: #{tail}, head: #{head}, minAvg: #{minAvg}, minIndex: #{minIndex}"
-  return minIndex
+
+  for i in 2..a.length - 1 do
+    avg = a[i-2] + a[i-1] + a[i]
+    if avg < min_3
+      min_3 = avg
+      min_3_index = i - 2
+    end
+  end
+
+  if min_2 * 3 <= min_3 * 2
+    return min_2_index
+  else
+    return min_3_index
+  end
 end
--- prefix_sums/min_avg_two_slice/ruby/solution_test.rb
+++ prefix_sums/min_avg_two_slice/ruby/solution_test.rb
@@ -8,7 +8,17 @@ describe 'solution' do
   end
 
   it 'finds minimal average of any slice of 2 or more elements' do
+    a = [-18, 65, -11, 73, -22, 90, 21, 10, 47, 87]
+    solution(a).must_equal 0
+  end
+
+  it 'finds minimal average of any slice of 2 or more elements' do
     a = [1, -1, 1, -1]
+    solution(a).must_equal 1
+  end
+
+  it 'finds minimal average of any slice of 2 or more elements' do
+    a = [1, -10, -1, 10]
     solution(a).must_equal 1
   end
 
