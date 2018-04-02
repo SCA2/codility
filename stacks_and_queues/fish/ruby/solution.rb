714382e3e89b1d20cf55c36202fdcac3af918bb8
SCA2 <tpryan_1965@yahoo.com>
2018-04-01 19:11:01 -0700

    fish ruby solution

--- stacks_and_queues/fish/ruby/solution.rb
+++ stacks_and_queues/fish/ruby/solution.rb
@@ -3,4 +3,15 @@
 # N voracious fish are moving along a river. Calculate how many fish are alive.
 
 def solution(a, b)
+  survivors = []
+  threats = []
+  a.zip(b).each do |f|
+    if f[1] == 1
+      threats.push f
+    else
+      threats.pop while threats[-1] && threats[-1][0] < f[0]
+      survivors.push f unless threats[-1]
+    end
+  end
+  survivors.length + threats.length
 end
--- stacks_and_queues/fish/ruby/solution_test.rb
+++ stacks_and_queues/fish/ruby/solution_test.rb
@@ -5,6 +5,12 @@ describe 'solution' do
   it 'calculates number of surviving fish' do
     a = [4, 3, 2, 1, 5]
     b = [0, 1, 0, 0, 0]
-    solution(a).must_equal(2)
+    solution(a, b).must_equal(2)
+  end
+
+  it 'calculates number of surviving fish' do
+    a = [0, 1]
+    b = [1, 1]
+    solution(a, b).must_equal(2)
   end
 end
