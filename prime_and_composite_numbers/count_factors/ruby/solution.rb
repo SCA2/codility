1774a83f1e7a595dd7297159fd95c962a089ebf8
SCA2 <tpryan_1965@yahoo.com>
2018-04-06 15:27:04 -0700

    count_factors ruby solution

--- prime_and_composite_numbers/count_factors/ruby/solution.rb
+++ prime_and_composite_numbers/count_factors/ruby/solution.rb
@@ -3,4 +3,6 @@
 # Count factors of given number n.
 
 def solution(n)
+  x = (n**0.5).floor
+  (1..x).reduce(0) { |s, i| n % i == 0 ? s += (i * i == n ? 1 : 2) : s }
 end
--- prime_and_composite_numbers/count_factors/ruby/solution_test.rb
+++ prime_and_composite_numbers/count_factors/ruby/solution_test.rb
@@ -2,5 +2,13 @@ require_relative('../../../test_helper')
 require_relative('solution')
 
 describe 'solution' do
-  
+  it 'counts factors of n' do
+    n = 24  # 1, 2, 3, 4, 6, 8, 12, 24
+    solution(n).must_equal(8)
+  end
+
+  it 'counts factors of n' do
+    n = 100 # 1, 2, 4, 5, 10, 20, 25, 50, 100
+    solution(n).must_equal(9)
+  end
 end
