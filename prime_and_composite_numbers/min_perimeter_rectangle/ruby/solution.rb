40f58398821b05461147d2cac912e374081a8141
SCA2 <tpryan_1965@yahoo.com>
2018-04-06 16:39:32 -0700

    min_perimeter_rectangle ruby solution

--- prime_and_composite_numbers/min_perimeter_rectangle/ruby/solution.rb
+++ prime_and_composite_numbers/min_perimeter_rectangle/ruby/solution.rb
@@ -1,1 +1,10 @@
 # Codility Lesson 10 - Prime and Composite Numbers
+# MinPerimeterRectangle
+# Find the minimal perimeter of any rectangle whose area equals N
+
+def solution(n)
+  r = (n**0.5).floor
+  (1..r).reduce(Float::INFINITY) do |perimeter, h|
+    n % h == 0 ? [perimeter, 2 * (h + n / h)].min : perimeter
+  end
+end
--- prime_and_composite_numbers/min_perimeter_rectangle/ruby/solution_test.rb
+++ prime_and_composite_numbers/min_perimeter_rectangle/ruby/solution_test.rb
@@ -2,5 +2,13 @@ require_relative('../../../test_helper')
 require_relative('solution')
 
 describe 'solution' do
-  
+  it 'finds minimum perimeter of rectangle with area n' do
+    n = 30
+    solution(n).must_equal(22)
+  end
+
+  it 'finds minimum perimeter of rectangle with area n' do
+    n = 1
+    solution(n).must_equal(4)
+  end
 end
