b42166561daa5766003ee5d7b8e52a631eaa4f3d
SCA2 <tpryan_1965@yahoo.com>
2018-05-21 17:28:43 -0700

    nailing planks ruby solution

--- binary_search/nailing_planks/ruby/solution.rb
+++ binary_search/nailing_planks/ruby/solution.rb
@@ -3,32 +3,50 @@
 # Count the minimum number of nails that allow a series of planks to be nailed.
 
 def solution(a, b, c)
-  planks = a.zip(b)
+  planks = a.zip(b).sort
   nails = c.each_with_index.sort
-  last_nail = -1
+  max_index = -1
   # puts "planks: #{planks}"
   # puts "nails: #{nails}"
 
+  last_plank = nil
   planks.each do |plank|
-    nail = nails.bsearch do |n|
+    nail_1 = [*nails.each_with_index].bsearch do |n, i|
       if n[0] > plank[1]
         -1
       elsif n[0] >= plank[0] && n[0] <= plank[1]
         0
-      else #if n[0] < plank[0]
+      else
         1
       end
     end
 
-    return -1 if nail.nil?
+    return -1 if nail_1.nil?
 
-    first_nail = nail[1]
-    min = nails.bsearch { |n| n[0] >= plank[0] }
-    first_nail = first_nail < min[1] ? first_nail : min[1]
-    # puts "plank: #{plank}, nail: #{nail}, min: #{min}"
-    last_nail = last_nail > first_nail ? last_nail : first_nail
+    nail_2 = [*nails.each_with_index].bsearch do |n, i|
+      n[0] >= plank[0]
+    end
 
+    # puts "nail_1: #{nail_1}, nail_2: #{nail_2}"
+
+    nail = nail_1[0][0] < nail_2[0][0] ? nail_1 : nail_2
+
+    nail_index = nail[1]
+
+    next if last_plank && last_plank[0] == plank[0]
+    # puts "plank: #{plank}, nail: #{nails[nail_index]}"
+
+    min_index = nails[nail_index][1]
+    nails[nail_index..-1].each do |nail|
+      break if nail[0] < plank[0] || nail[0] > plank[1]
+      # puts "nail: #{nail}"
+      min_index = nail[1] < min_index ? nail[1] : min_index
+      break if min_index <= max_index
+    end
+    max_index = max_index > min_index ? max_index : min_index
+
+    last_plank = plank
   end
 
-  return last_nail + 1
+  return max_index + 1
 end
