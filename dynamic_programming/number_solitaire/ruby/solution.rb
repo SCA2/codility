acab29e5f36bb8c572121ead5b173c931d477596
SCA2 <tpryan_1965@yahoo.com>
2018-06-12 17:07:12 -0700

    number solitaire ruby solution

--- dynamic_programming/number_solitaire/ruby/solution.rb
+++ dynamic_programming/number_solitaire/ruby/solution.rb
@@ -3,4 +3,13 @@
 # In a given array, find the subset of maximal sum in which the distance between consecutive elements is at most 6.
 
 def solution(a)
+  d = 6
+  n = a.length
+  sums = [a[0]] * d
+
+  for i in 1...n do
+    sums[i % d] = sums.max + a[i]
+  end
+
+  sums[(n-1) % d]
 end
--- dynamic_programming/number_solitaire/ruby/solution_test.rb
+++ dynamic_programming/number_solitaire/ruby/solution_test.rb
@@ -1,9 +1,14 @@
-require_relative('../../../tet_helper')
+require_relative('../../../test_helper')
 require_relative('solution')
 
 describe 'solution' do
   it 'finds the subset of maximal sum' do
     a = [1, -2, 0, 9, -1, -2]
     solution(a).must_equal(8)
+  end
+
+  it 'finds the subset of maximal sum' do
+    a = [1, 1]
+    solution(a).must_equal(2)
   end
 end
